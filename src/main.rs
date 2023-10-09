5ee8963d0f12f4a5180687a33c7cd4b847917869
Richard Tung <zqdbyct@gmail.com>
2023-10-09 23:38:52 +0800

    Crossbeam multithreaded mandelbrot set image gen

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use num::Complex;
 use crate::image_writer::write_image;
 use crate::parse_complex_util::parse_complex;
 use crate::parse_pair_util::parse_pair;
+use crate::pixel_to_point_util::pixel_to_point;
 use crate::renderer::render;
 
 fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
@@ -34,6 +35,22 @@ fn main() {
     let lower_right = parse_complex(&args[4]).expect("error parsing lower left corner point");
 
     let mut pixels = vec![0; bounds.0 * bounds.1];
-    render(&mut pixels, bounds, upper_left, lower_right);
+    let threads = 8;
+    let rows_per_band = bounds.1 / threads + 1;
+    {
+        let bands: Vec<&mut [u8]> = pixels.chunks_mut(rows_per_band * bounds.0).collect();
+        crossbeam::scope(|spawner| {
+            for (i, band) in bands.into_iter().enumerate() {
+                let top = rows_per_band * i;
+                let height = band.len() / bounds.0;
+                let band_bounds = (bounds.0, height);
+                let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
+                let band_lower_right = pixel_to_point(bounds, (bounds.0, top + height), upper_left, lower_right);
+                spawner.spawn(move |_| {
+                    render(band, band_bounds, band_upper_left, band_lower_right);
+                });
+            }
+        }).unwrap();
+    }
     write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
 }
